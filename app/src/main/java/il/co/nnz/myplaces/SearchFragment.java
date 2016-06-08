4c030c497a902853ab99f03d3f9a48a1a506fd75
nirushz <nirushz@gmail.com>
2016-06-08 16:02:43 +0300

    version1

--- app/src/main/java/il/co/nnz/myplaces/SearchFragment.java
+++ app/src/main/java/il/co/nnz/myplaces/SearchFragment.java
@@ -3,9 +3,12 @@ package il.co.nnz.myplaces;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /**
@@ -27,5 +30,31 @@ public class SearchFragment extends Fragment {
     }
 
 
+    public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.MyViewHolder> {
 
+        @Override
+        public PlaceAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(PlaceAdapter.MyViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+
+        public class MyViewHolder extends RecyclerView.ViewHolder {
+
+            private TextView textName, textAddress, textDistane;
+            private ImageView imagePlace;
+
+            public MyViewHolder(View itemView) {
+                super(itemView);
+            }
+        }
+    }
 }
